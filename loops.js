753827f31b417bcb1bd770ed78376cbf13528f22
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-01 01:09:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,7 +6,8 @@
   console.log("Hello World the " + i + " time")
 }*/
 
-for (let i = 0 ; i < 25; i++) {
+function forLoop(i) {
+  for (let i = 0 ; i < 25; i++) {
   if (i === 1) {
     console.log("I am 1 strange loop.") 
   } else {
