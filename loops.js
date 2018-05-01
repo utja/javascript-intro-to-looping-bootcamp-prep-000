74377e43c07661c00c12954c7c54b84d745df023
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-01 01:58:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,7 +26,7 @@ function whileLoop(number) {
 
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() > = 0.5
+    return Math.random() >= 0.5
   }
   do {
     array.pop
