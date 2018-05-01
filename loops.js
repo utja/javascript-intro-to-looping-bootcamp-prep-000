42ffb101c1eacff4eaf8fe0317fc272508c95862
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-01 01:21:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,12 +8,12 @@
 
 function forLoop(array) {
   for (let i = 0 ; i < 25; i++) {
-  if (i === 1) {
-    console.log("I am 1 strange loop.")
-  } else if (i === 0) {
-    console.log("I am 0 strange loop.")
+  if (i === 0) {
+    array.push("I am 0 strange loop.")
+  } else if (i === 1) {
+    array.push("I am 1 strange loop.")
   } else {
-    console.log(`I am ${i} strange loops.`)
+    array.push(`I am ${i} strange loops.`)
   }
 }
 }
